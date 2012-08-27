9f6537d74ddd2dad9c0387ed837db48591acfedf
avjgit <avjmail@gmail.com>
2012-08-27 21:13:12 +0300

    ruby.learncodethehardway.org 12/52 more printing...

--- learn-ruby-the-hard-way.rb
+++ learn-ruby-the-hard-way.rb
@@ -1,3 +1,65 @@
 # http://ruby.learncodethehardway.org/book/
 
 # An 'octothorpe' is also called a 'pound', 'hash', 'mesh', or any number of names. Pick the one that makes you chill out.
+
+cars = 100
+people = 100
+puts "there are #{cars} cars for #{people} available."
+puts "there are %d cars for %d people available." % [cars, people]
+
+x = "there are #{10} types of people"
+binary = "binary"
+do_not = "don't"
+y = "Those who know #{binary} and those who #{do_not}"
+puts x
+puts y
+
+puts "I said: #{x}."
+puts "I also said: '#{y}'."
+
+hilarious = false
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+puts joke_evaluation
+
+w = "this is the left side of ..."
+e = "a string with a right side."
+puts w+e
+
+formatter = "%s %s %s %s "
+
+puts formatter % [1, 2, 3, 4]
+puts formatter % ['one', 'two', 'three', 'four']
+puts formatter % [true, false, false, true]
+puts formatter % [formatter, formatter, formatter, formatter]
+
+months = "Jan\nFeb"
+puts months
+
+#hm, quite interesting stuff
+puts <<PARAGRAPH
+asdf
+sdfadsasdadsf
+wewe
+asda
+PARAGRAPH
+
+puts "I am 6'2\" tall"
+puts 'I am 6\'2" tall'
+
+tabbed = "\tI'm tabbed in."
+split = "I'm split\non a line"
+backslashed = "I'm \\ a \\ cat"
+lines = <<wtf
+let's make a list:
+\t* food
+\t* fish
+\t* else\n\t* grass
+wtf
+puts tabbed
+puts split
+puts backslashed
+puts lines
+
+print 'hey, what''s up?'
+mood = gets
+puts 'ok, I got it - ' + mood
