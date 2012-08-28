259aea455fc992065c46661892044c91ef217309
avjgit <avjmail@gmail.com>
2012-08-28 06:55:44 +0300

    ruby.learncodethehardway.org 17/52 files copying

--- learn-ruby-the-hard-way.rb
+++ learn-ruby-the-hard-way.rb
@@ -106,30 +106,47 @@ puts "Here is your file: #{filename}"
 puts txt.read()
 txt.close()
 
-puts "i am going to delete #{filename}"
-puts "if you don't want it, press Ctrl+C"
-puts "otherwise hit Enter"
-print "? "
-STDIN.gets
+# puts "i am going to delete #{filename}"
+# puts "if you don't want it, press Ctrl+C"
+# puts "otherwise hit Enter"
+# print "? "
+# STDIN.gets
 
-puts 'openining the file'
-target = File.open(filename, 'w')
+# puts 'openining the file'
+# target = File.open(filename, 'w')
 
-puts 'truncating the file. bye file!'
-target.truncate(target.size)
+# puts 'truncating the file. bye file!'
+# target.truncate(target.size)
 
-puts 'now I''m going to ask you for 3 lines'
-print 'line 1: '; line1 = STDIN.gets.chomp()
-print 'line 2: '; line2 = STDIN.gets.chomp()
-print 'line 3: '; line3 = STDIN.gets.chomp()
-puts 'I''m going to write them into the file'
+# puts 'now I''m going to ask you for 3 lines'
+# print 'line 1: '; line1 = STDIN.gets.chomp()
+# print 'line 2: '; line2 = STDIN.gets.chomp()
+# print 'line 3: '; line3 = STDIN.gets.chomp()
+# puts 'I''m going to write them into the file'
 
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
+# target.write(line1)
+# target.write("\n")
+# target.write(line2)
+# target.write("\n")
+# target.write(line3)
+# target.write("\n")
 
-puts "And finally we close it"
-target.close()
+# puts "And finally we close it"
+# target.close()
+
+from_file, to_file = ARGV
+puts "Copying from #{from_file} to #{to_file}"
+
+input = File.open(from_file)
+indata = input.read()
+puts "the input file is #{indata.length} bytes long"
+
+puts "does output file exists? #{File.exists? to_file}"
+
+output = File.open(to_file, 'w')
+output.write(indata)
+
+puts 'alright, all done'
+output.close()
+input.close()
+
