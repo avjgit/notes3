710b7d5753feccc00ff5e7c93617216108343166
avjgit <avjmail@gmail.com>
2012-08-28 22:47:15 +0300

    ruby.learncodethehardway.org 20/52 functions

--- learn-ruby-the-hard-way.rb
+++ learn-ruby-the-hard-way.rb
@@ -165,3 +165,26 @@ end
 puts ok_usual_function("A", "B")
 puts unusual_argument("A", "B")
 
+input_file = '\asdf.txt'
+
+def print_all(f)
+	puts f.read()
+end
+def rewind(f)
+	f.seek(0, IO::SEEK_SET)
+end
+def print_a_line(line_count, f)
+	puts "#{line_count} #{f.readline()}"
+end
+current_file = File.open(input_file)
+puts "First let's print the whole file:"
+print_all(current_file)
+puts "Now let's rewind"
+rewind(current_file)
+puts "Let's print 3 lines:"
+current_line = 1
+print_a_line(current_line, current_file)
+current_line += 1
+print_a_line(current_line, current_file)
+current_line += 1
+print_a_line(current_line, current_file)
