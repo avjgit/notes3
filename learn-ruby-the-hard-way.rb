57624ef3ca2e6db16f0d3378b1cbeb270bfd9a54
avjgit <avjmail@gmail.com>
2012-08-28 06:45:55 +0300

    ruby.learncodethehardway.org 16/52 files IO

--- learn-ruby-the-hard-way.rb
+++ learn-ruby-the-hard-way.rb
@@ -105,3 +105,31 @@ txt = File.open(filename)
 puts "Here is your file: #{filename}"
 puts txt.read()
 txt.close()
+
+puts "i am going to delete #{filename}"
+puts "if you don't want it, press Ctrl+C"
+puts "otherwise hit Enter"
+print "? "
+STDIN.gets
+
+puts 'openining the file'
+target = File.open(filename, 'w')
+
+puts 'truncating the file. bye file!'
+target.truncate(target.size)
+
+puts 'now I''m going to ask you for 3 lines'
+print 'line 1: '; line1 = STDIN.gets.chomp()
+print 'line 2: '; line2 = STDIN.gets.chomp()
+print 'line 3: '; line3 = STDIN.gets.chomp()
+puts 'I''m going to write them into the file'
+
+target.write(line1)
+target.write("\n")
+target.write(line2)
+target.write("\n")
+target.write(line3)
+target.write("\n")
+
+puts "And finally we close it"
+target.close()
