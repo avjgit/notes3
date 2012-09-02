f4ebc9c8935ed4cde160c3b02cc3e0ed6e30070f
avjgit <avjmail@gmail.com>
2012-09-02 10:23:28 +0300

    ruby.hasbrains.ru 15/50: symbols, hashes, initialize

--- ruby.hasbrains.ru.rb
+++ ruby.hasbrains.ru.rb
@@ -8,8 +8,11 @@
 # camelcase for classes
 class Item
 
-	def initialize
-		@price = 30
+	def initialize(options={:price => 10, :weight => 0.1})
+		if options
+			@price = options[:price]
+			@weight = options[:weight]
+		end
 	end
 
 	# # getter
@@ -39,11 +42,24 @@ item1.price = 10
 
 item2 = Item.new
 item2.price = 20
-item3 = Item.new
+item3 = Item.new({:price => 12.34, :weight => 2.5})
 
 p item1 #<Item:0x1327e30>
 p item2
+p item3
 
 p item1.price
 p item2.price
 p item3.price
+
+
+#string vs symbols:
+# in irb:
+# 001 "ruby".object_id #A
+# 002 "ruby".object_id #B
+# 003 :ruby.object_id #C
+# 004 :ruby.object_id #C
+
+my_hash = {:name => 'James', :family_name => 'Bond'}
+
+p my_hash[:name]
