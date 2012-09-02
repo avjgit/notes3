99b87fe2610b0d11d12cc29dd83e6ae5b6ae1198
avjgit <avjmail@gmail.com>
2012-09-02 10:04:35 +0300

    ruby.hasbrains.ru 13/50: get, set, initialize

--- ruby.hasbrains.ru.rb
+++ ruby.hasbrains.ru.rb
@@ -7,17 +7,35 @@
 
 # camelcase for classes
 class Item
+
+	def initialize
+		@price = 30
+	end
+
+	# getter
 	def price
 		# 100
-		rand(100)
+		# rand(100)
+	@price
+	end
+
+	#setter
+	def price=(price_value)
+		@price = price_value
 	end
 end
 
 item1 = Item.new
+# item1.price=(10) @what is really going on
+item1.price = 10
+
 item2 = Item.new
+item2.price = 20
+item3 = Item.new
 
 p item1 #<Item:0x1327e30>
 p item2
 
 p item1.price
 p item2.price
+p item3.price
