d6b9a841cc0576ffe27e4267d86ea412a1cf5494
avjgit <avjmail@gmail.com>
2012-09-08 09:09:38 +0300

    LU programming I lab 5.3

--- lu/programming_i/lab5.3.cpp
+++ lu/programming_i/lab5.3.cpp
@@ -5,12 +5,14 @@ using namespace std;
 
 int main()
 {
+    //inner cycle
     for (int i=1; i<=4; i++)
     {
 //        Sets the number of characters to be used as the field width for the next insertion operation.
         for (int k=1; k<=4; k++) cout << setw(4) << i*k;
         cout << endl;
     };
+    
     cout << endl;
     system("pause");
     return 0;
