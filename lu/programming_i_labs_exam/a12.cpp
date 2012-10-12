10aafb90f156c2c5facd1c3ff0733af1b43313a4
avjgit <avjmail@gmail.com>
2012-10-12 20:42:47 +0300

    LU programming I labs exam a12 init

---
+++ lu/programming_i_labs_exam/a12.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+// Prasības:
+// 01) Ievadot loģiski nepareizus datus, programmai jāizdod atbilstošs paziņojums.
+// 02) Jābūt iespējai programmu izpildīt atkārtoti.
+// Atrādot programmu, jāatrāda
+// a) programmas teksts,
+// b) testa piemēri,
+// c) programma darbībā.
+// Precīzākas prasības sk. Laboratorijas darbu noteikumos.
+// A12. Dots naturāls skaitlis n.
+// Izdrukāt skaitli, kurš iegūts no dotā skaitļa, izmetot ciparu k.
+// (Piemēram, ja n=12025 un k=2, jāiegūst 105).
+// Skaitļa dalīšana ciparos jāveic skaitliski.
+
+int number_order(const int number)
+{
+    const int base = 10;
+    int order = 0;
+    while (pow(base, order) <= number) order++;
+    return --order;
+}
+int main()
+{
+    int number;
+    cout << "Enter positive integer: ";
+    cin >> number;
+    const int order = number_order(number);
+    int *number_split = new int[number_order];
+
+    // while(order >= 0)
+    // {
+    //     number_split[order] = number/
+    // }
+
+    system("pause");
+    return 0;
+}
