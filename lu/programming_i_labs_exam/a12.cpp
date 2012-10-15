40e6ebe358c7ca7d06bbb1ae570e1838011bdfa1
avjgit <avjmail@gmail.com>
2012-10-15 20:05:21 +0300

    LU programming I labs exam a12 int_to_array

--- lu/programming_i_labs_exam/a12.cpp
+++ lu/programming_i_labs_exam/a12.cpp
@@ -14,6 +14,16 @@ using namespace std;
 // (Piemēram, ja n=12025 un k=2, jāiegūst 105).
 // Skaitļa dalīšana ciparos jāveic skaitliski.
 
+// print out a question and get an input
+int request(string question)
+{
+    int answer;
+    cout << endl << question;
+    cin >> answer;
+    return answer;
+}
+
+// gets order of a number (eg, for 98765 it'll return 4)
 int number_order(const int number)
 {
     const int base = 10;
@@ -29,48 +39,62 @@ int number_order(const int number)
 // or from 987 substract 980
 int get_nth_order_digit(const int number, int n )
 {
-    cout << endl << "for n " << n << ": ";
-    const int base = 10;                                   // counting base (decimal)
-    double order = pow(base, n);
-    cout << endl << "foundation is " << order;
-    cout << endl << "foundation is " << (int)pow(base, n);
-
-    int upper_bound =  number/ (int)order;                 // with 98765 and n=2, gets 987
-    cout << endl << "upper bound is " << upper_bound;
-
-    double order_low = pow(base, n+1);
-    int lower_bound = (number/ (int)order_low) * base; // with 98765 and n=2, gets 980
-    cout << endl << "lower bound is " << lower_bound;
-
-    return (int)(upper_bound - lower_bound);                      // with 98765 and n=2, returns 987-980
+    const int base = 10;                                    // counting base (decimal)
+    double order = pow(base, n);                            // on which order (eg, 100 or 10000) to search
+    int upper_bound =  number/ (int)order;                  // with 98765 and n=2, gets 987
+    int lower_bound = (number/ (int)(order * base)) * base; // with 98765 and n=2, gets 980
+                                                            // complicated casting is because of issues with types
+    // cout << endl << "for n " << n << ": ";
+    // cout << endl << "foundation is " << order;           // output for debugging purposes
+    // cout << endl << "upper bound is " << upper_bound;
+    // cout << endl << "lower bound is " << lower_bound;
+    // cout << endl << (upper_bound - lower_bound);
+    return upper_bound - lower_bound;                       // with 98765 and n=2, returns 987-980
 }
 
-// print out a question and get an input
-int request(string question)
+// splits number to array, according to order of each digit, counting from right
+int* int_to_array(int number)
 {
-    int answer;
-    cout << endl << question;
-    cin >> answer;
-    return answer;
+    int order = number_order(number);   // order of a number (how long it is)
+    int *digits = new int[order+2];     // array to store number's digits; +1 cell for '\0'
+    digits[order+1] = '\0';             // setting last element of the array
+                                        // fill in array with digits
+    while(order >= 0) digits[order--] = get_nth_order_digit(number, order);
+    return digits;
 }
+
+
 int main()
 {
     int number;
     // number = request("Enter positive integer to process: ");
     int removable;
     // removable = request("Enter digit you want to remove: ");
-    number = 654321;
-    int order = number_order(number);   // order of number (how long it is)
-    int *digits = new int[order+1];     // array to store number's digits; +1 cell for '\0'
+    number = 987654;
 
-    // fill in array with digits
-    for (int i = order; i >= 0; i--)
+    // int order = number_order(number);   // order of a number (how long it is)
+    // int *digits = new int[order+1];     // array to store number's digits; +1 cell for '\0'
+
+    // // fill in array with digits
+    // for (int i = order; i >= 0; i--)
+    // {
+    //     digits[i] = get_nth_order_digit(number, i);
+    // }
+
+    int *digits = int_to_array(number);
+    // int digit = int_to_array(number);
+
+    // output for debugging purposes
+    // for (int i = 0; i <= 6; i++) cout << endl << "digit " << i << " is " << digits[i];
+    int i = 0;
+    while (digits[i] != '\0')
     {
-        digits[i] = get_nth_order_digit(number, i);
+        cout << endl << "digit " << i << " is " << digits[i];
+        i++;
     }
 
-    // for (int i = order; i >= 0; i--) cout << endl << "digit " << order-i+1 << " is " << digits[i];
-    // for (int i = 0; i <= order; i++) cout << endl << "digit " << i << " is " << digits[i];
+
+
 
     // int *output = new int [order];
     // int j = 0;
--- lu/programming_i_labs_exam/tests.cpp
+++ lu/programming_i_labs_exam/tests.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 #include <math.h>
 using namespace std;
+
+int number_order(const int number)
+{
+    const int base = 10;
+    int order = 0;
+    while (pow(base, order) <= number) order++;
+    return --order;
+}
+
 int main()
 {
-    cout << (int)pow(10, 4);
+    // int arr[5] = {};
+    cout << number_order(12345);
+
     system("pause");
     return 0;
 }
