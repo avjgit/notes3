fabdf2c23af0b284bae98a9fe43743fc00373098
avjgit <avjmail@gmail.com>
2012-10-27 15:44:25 +0300

    LU programming I labs exam c15 array of params

--- lu/programming_i_labs_exam/c15.cpp
+++ lu/programming_i_labs_exam/c15.cpp
@@ -7,14 +7,17 @@
 // ----------------------------------------------
 #include "utils.h"
 
+const int parameters = 3;
+
 int main()
 {
     int a, b, c;
+    int line[parameters];
     cout << "Enter ax + by = c line's parameters, as integers a b c: ";
-    cin >> a;
-    cin >> b;
-    cin >> c;
-    cout << "Your input is being interpreted as " << a << ", " << b << " and " << c << endl;
+    cin >> line[0];
+    cin >> line[1];
+    cin >> line[2];
+    cout << "Your input is being interpreted as " << line[0] << ", " << line[1] << " and " << line[2] << endl;
     system("pause");
     return 0;
 }
