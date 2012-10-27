84d9cd7f6ef2ab9bd7d0f85512b1a0b2a6b5efce
avjgit <avjmail@gmail.com>
2012-10-27 16:49:29 +0300

    LU programming I labs exam c15 hardcoding removed

--- lu/programming_i_labs_exam/c15.cpp
+++ lu/programming_i_labs_exam/c15.cpp
@@ -24,9 +24,9 @@ int* get_line()
     while (is_line_wrong(line))
     {
         cout << "Enter line's ax + by = c parameters, as space-separated integers a b c: ";
-        cin >> line[0];
-        cin >> line[1];
-        cin >> line[2];
+        cin >> line[parameter_a];
+        cin >> line[parameter_b];
+        cin >> line[parameter_c];
 
         if (is_line_wrong(line))
             cout << "You have entered incorrect parameter B. It can not be 0." << endl;
@@ -36,8 +36,6 @@ int* get_line()
 
 int main()
 {
-    // int line[max_number_of_lines][parameters];
-
     // http://stackoverflow.com/questions/936687/how-do-i-declare-a-2d-array-using-new
     int** lines = new int*[max_number_of_lines];
     int real_number_of_lines = 0;
@@ -47,8 +45,10 @@ int main()
         lines[line] = get_line();
     }
 
-    // line[0] = get_line();
-    cout << "Your input is being interpreted as " << lines[0][0] << ", " << lines[0][1] << " and " << lines[0][2] << endl;
+    cout << "Your input is being interpreted as "
+        << lines[0][parameter_a] << ", "
+        << lines[0][parameter_b] << " and "
+        << lines[0][parameter_c] << endl;
     cout << "Your enetered " << real_number_of_lines << " lines. ";
     system("pause");
     return 0;
