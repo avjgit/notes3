752060f71349efd543977e5de1071d706bd31ccd
avjgit <avjmail@gmail.com>
2012-10-27 16:31:58 +0300

    LU programming I labs exam c15 wtf

--- lu/programming_i_labs_exam/c15.cpp
+++ lu/programming_i_labs_exam/c15.cpp
@@ -8,7 +8,7 @@
 #include "utils.h"
 
 const int parameters = 3;
-const int max_number_of_lines = 2;
+const int max_number_of_lines = 1;
 const int parameter_a = 0;
 const int parameter_b = 1;
 const int parameter_c = 2;
@@ -21,32 +21,44 @@ bool is_line_wrong(int line[parameters])
 int* get_line()
 {
     int line[parameters] = {0, 0, 0};
-    while (is_line_wrong(line))
-    {
+    // while (is_line_wrong(line))
+    // {
         cout << "Enter line's ax + by = c parameters, as space-separated integers a b c: ";
         cin >> line[0];
         cin >> line[1];
         cin >> line[2];
 
-        if (is_line_wrong(line))
-            cout << "You have entered incorrect parameter B. It can not be 0." << endl;
-    }
+
+    cout << endl << "Your input is being interpreted as "
+        << line[0] << ", "
+        << line[1] << " and "
+        << line[2] << endl;
+
+        // if (is_line_wrong(line))
+            // cout << "You have entered incorrect parameter B. It can not be 0." << endl;
+    // }
     return line;
 }
 
 int main()
 {
-    // int line[max_number_of_lines][parameters];
-
     // http://stackoverflow.com/questions/936687/how-do-i-declare-a-2d-array-using-new
     int** lines = new int*[max_number_of_lines];
     int real_number_of_lines = 0;
 
-    for (int line = 0; line < max_number_of_lines; line++, real_number_of_lines++)
+    for (int line = 0; line < max_number_of_lines; line++)
+    {
         lines[line] = get_line();
 
-    // line[0] = get_line();
-    cout << endl << "Your input is being interpreted as " << lines[0][0] << ", " << lines[0][1] << " and " << lines[0][2] << endl;
+    cout << endl << "Your input is being interpreted as "
+        << lines[0][0] << ", "
+        << lines[0][1] << " and "
+        << lines[0][2] << endl;
+    }
+    cout << endl << "Your input is being interpreted as "
+        << lines[0][0] << ", "
+        << lines[0][1] << " and "
+        << lines[0][2] << endl;
     cout << endl << "You have entered " << real_number_of_lines << " lines.";
     system("pause");
     return 0;
