781831f3a92fa5efafe36f4852afa4ca6ab359f2
avjgit <avjmail@gmail.com>
2012-10-29 22:10:07 +0200

    LU programming I labs exam d7 print reuse

--- lu/programming_i_labs_exam/d7.cpp
+++ lu/programming_i_labs_exam/d7.cpp
@@ -9,6 +9,7 @@
 // Šīs programmas ietvaros NAV obligāti jānodrošina tāda lietotāja saskarne,
 // kāda tika prasīta iepriekšējos trīs uzdevumos (main funkciju drīkst “cieti” iekodēt).
 // Citas prasības sk. Laboratorijas darbu noteikumos.
+
 // D7. Izveidot klasi "Skaitļu rinda" (queue, pa vienu pusi ieliek, pa otru izņem!),
 // kurā glabājas masīvs ar skaitliskām vērtībām (int) garumā pieci
 // un rindas elementu skaits.
@@ -18,9 +19,9 @@
 // kurš paziņo par objekta likvidēšanu
 // un likvidēto elementu skaitu
 // (ja likvidēšanas brīdī rinda nav tukša),
-// (3) metode "enqueue", kas pievieno rindai elementu,
+// (3) metode "enqueu", kas pievieno rindai elementu,
 // ja tā nav pilna,
-// (4) metode "dequeue", kas izņem no rindas elementu un atgriež tā vērtību,
+// (4) metode "dequeu", kas izņem no rindas elementu un atgriež tā vērtību,
 // (5) metode "Count", kas atgriež elementu skaitu rindā,
 // (6) metode "IsEmpty", kas noskaidro, vai rinda ir tukša.
 // ----------------------------------------------
@@ -29,83 +30,118 @@
 class number_queue
 {
     static const int queue_size = 5;
+    int queue[queue_size];
+    int size;
 
 public:
-    int list[queue_size];
-    int amount;
 
     number_queue()
     {
-        amount = 0;
+        size = 0;
+    }
+
+    number_queue(int _size, int* elements)
+    {
+        for(int i = 0; i < _size; i++)
+            enqueue(elements[i]);
     }
 
     ~number_queue()
     {
-        cout << endl << "Queue is being destructed. ";
-        if (amount > 0)
-            cout << endl << "There were " << amount << " elements." << endl;
+        print("Queue was destructed.");
+        if (size > 0)
+            cout << endl << "There were " << size << " elements." << endl;
     }
 
     void print()
     {
-        cout << "Queue:";
-        for (int i = 0; i < amount; i++)
-            cout << " " << list[i];
-        cout << endl;
+        if (is_empty())
+        {
+            print("Queue is empty.");
+        }
+        else
+        {
+            cout << "Queue:";
+            for (int i = 0; i < size; i++)
+                cout << " " << queue[i];
+            cout << endl;
+        }
+    }
+
+    void print(string message)
+    {
+        cout << endl << message << endl;
     }
 
     void enqueue(int element)
     {
-        if (amount < queue_size)
+        if (size < queue_size)
         {
-            list[amount++] = element;
+            queue[size++] = element;
         }
         else
         {
-            cout << endl << "Queue is full, enqueuing not possible.";
+            print("Queue is full, enqueuing not possible.");
         }
     }
 
     int dequeue()
     {
-        int dequeable = list[0];
-        for (int i = 0; i < amount; i++)
-            list[i] = list[i+1];
-        amount--;
-        return dequeable;
+        int dequeueable = queue[0];
+        for (int i = 0; i < size; i++)
+            queue[i] = queue[i+1];
+        size--;
+        return dequeueable;
+    }
+
+    int count()
+    {
+        return size;
+    }
+
+    bool is_empty()
+    {
+        return size == 0;
     }
 };
 
 void d7()
 {
     const char stop = 'x';
-    const char push = '+';
-    const char pull = '-';
+    const char enqueue = '+';
+    const char dequeue = '-';
 
-    number_queue queue;
+    number_queue q;
     int input;
 
     char command = ' ';
     int out;
     while (command != stop)
     {
-        print("Enter desired action: ");
+        print("Enter +a to enqueue a, - to dequeue, x to stop: ");
         cin >> command;
-        if (command == push)
+        if (command == enqueue)
         {
             cin >> input;
-            queue.enqueue(input);
-            queue.print();
+            q.enqueue(input);
+            q.print();
         }
-        else if (command == pull)
+        else if (command == dequeue)
         {
-            out = queue.dequeue();
-            cout << out << " dequeued." << endl;
-            queue.print();
+            if (!q.is_empty())
+            {
+                out = q.dequeue();
+                cout << out << " dequeued." << endl;
+                q.print();
+            }
+            else
+            {
+                print("Queue is empty, dequeuing not possible.");
+            }
         }
         else if (command != stop)
         {
-            cout << "you input is incorrect";
+            print("Your input is incorrect");
         }
     }
     print("Program is over. ");
